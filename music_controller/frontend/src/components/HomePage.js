b2d56bf209fbab36ae561ddf8d93f3aa7ef70b95
wlgud0402 <wlgudrlgus@naver.com>
2021-02-03 17:38:44 +0900

    making room with api, fetch, history, match.params

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ export default class HomePage extends Component {
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
         </Switch>
       </Router>
     );
--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', index),
     path('join', index),
     path('create', index),
-    path('join/<int:pk>', index),
+    path('room/<str:roomCode>', index),
 ]
